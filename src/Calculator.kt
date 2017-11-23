a1a2fdc6a5152df0c1c0b8570a4ca13d725a8040
Evan <evanmyersflow@gmail.com>
2017-11-23 03:31:02 -0800

    Idek at this point

--- src/Calculator.kt
+++ src/Calculator.kt
@@ -1,47 +1,88 @@
-import Points.Companion.input
 import java.util.*
+import currentPoints.Companion.input
+
+//change input to int, don't know how to
+
+import currentPoints.Companion.x
+import Grade.Companion.y
+import kotlin.collections.Array
 
 val scanner = Scanner(System.`in`)
-val possibleGrades = listOf("A", "B", "C", "D", "F")
+val possibleGrades = listOf("A", "B", "C", "D")
 const val SEPERATOR = ","
+val math = Array
+
+//I know array isn't the right collection, don't know what the right one is
+
+val input = scanner.nextLine().toUpperCase().trim()
 
 fun main(args: Array<String>) {
 
     println("Enter your total points from synergy (ex: 345)")
 
-    loop@ while (true) {
-        val input = scanner.nextLine()
-    }
-    val (currentPoints, desiredGrade) = try {
-        input.split(SEPERATOR).let {
-            Points.parse(first().trim()) to Grade.parse(last().trim())
-        }
-    } catch (e: IllegalStateException) {
-        println("Those entries will not work")
-        continue@loop
+    while (true) {
+        printMath(math)
+        printPossibleGrades()
     }
 }
 
-data class Points(val x: Int) {
+fun printPossibleGrades() {
+    if (possibleGrades.contains(input)) {
+        when (possibleGrades) {
+            "A" -> 9 / 10
+            "B" -> 8 / 10
+            "C" -> 7 / 10
+            "D" -> 6 / 10
+        }   else {
+            throw -> IllegalStateException("That's not a grade!")
+        }
+
+    }
+}
+
+val (currentPoints) = try {
+    input.split(SEPERATOR).let {
+        Points.parse(first().trim()) to Grade.parse(last().trim())
+    }
+} catch (e: IllegalStateException) {
+    println("Those entries will not work")
+    continue@loop
+}
+
+data class currentPoints(val x: Int) {
     companion object {
-        fun parse(input: String): Points {
+        fun parse(input: String): currentPoints {
             if (input.length != 1 || input.length != 2 || input.length != 3)
                 throw IllegalStateException()
+            val x = input
+            return currentPoints(x)
         }
-        val x = input
 
-        return Points(x)
     }
 }
 
-data class Grade(val x: Int) {
+
+val (Grade) = try {
+    input.split(SEPERATOR).let {
+        Points.parse(first().trim()) to Grade.parse(last().trim())
+    }
+} catch (e: IllegalStateException) {
+    println("Those entries will not work")
+    continue@loop
+}
+
+data class Grade(val y: Int) {
     companion object {
         fun parse(input: String): Grade {
             if (input.length != 1 || input.length != 2 || input.length != 3)
                 throw IllegalStateException()
+            val y = input
+            return Grade(y)
         }
-        val x = possibleGrades.indexOf(input.last().toString())
 
-        return Grade(input.last)
     }
+}
+
+private fun printMath() {
+    x / 10 * 50 / 0.1 - 50 / 0.1 * y / 366 * 9 / 10
 }
